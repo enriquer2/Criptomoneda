09b60ba5c2de85faec67ecda2ebeaa107a6b1e5a
enriquer2 <ecases@alfatecsistemas.es>
2018-11-22 09:12:08 +0100

    create file structure for the project

---
+++ src/header/header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  constructor(props){
+    super(props);
+
+    let firstName = this.props.name.split(" ")[0];
+
+    this.state = {
+      name: firstName
+    }
+  }
+  render() {
+    return (
+      <h1>Hello, {this.state.name}!</h1>
+    );
+  }
+}
+
+export default Header;
