6095e12ac0a0e5ed2aca48155b5d36d5425c0164
enriquer2 <ecases@alfatecsistemas.es>
2018-11-28 19:29:28 +0100

    add style in coin list

--- src/body/coin.js
+++ src/body/coin.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './coin.css';
 import database from '../data/data.json';
 import More_info from './more_info/more_info';
 
@@ -9,30 +10,49 @@ class Coin extends Component {
   }
   render() {
     return (
-      <div>
-        <ul>
-          {
-            database.map(function(cripto){
-              return (
-                <li>
+      <div class="coin_list">
+        {
+          database.map(function(cripto){
+            return (
+              <ul>
+                <div class="row">
                   <h4>
                     {cripto.name} - {cripto.symbol}
                   </h4>
-                  Volume: {cripto.volume} Market Cap: {cripto.market_cap}
-                  <br />
-                  Value:
-                  <div>
-                    Open: {cripto.open} High: {cripto.high} Low: {cripto.low} Close: {cripto.close}
+                </div>
+                <div class="coin_info">
+                  <div class="row">
+                    <strong>Volume</strong>: {cripto.volume}
                   </div>
-                  <div>
+                  <div class="row">
+                    <strong>Market Cap</strong>: {cripto.market_cap}
+                  </div>
+                  <div class="row">
+                    Last values:
+                  </div>
+                  <div class="values">
+                    <div class="row">
+                      <strong>Open</strong>: {cripto.open}
+                    </div>
+                    <div class="row">
+                      <strong>High</strong>: {cripto.high}
+                    </div>
+                    <div class="row">
+                      <strong>Low</strong>: {cripto.low}
+                    </div>
+                    <div class="row">
+                      <strong>Close</strong>: {cripto.close}
+                    </div>
+                  </div>
+                  <div class="row">
                     <More_info name={cripto.symbol}/>
                   </div>
-                  <br></br>
-                </li>
-              )
-            })
-          }
-        </ul>
+                </div>
+                <br></br>
+              </ul>
+            )
+          })
+        }
       </div>
     );
   }
