0d0e4868aae02d8f4d76dc371d74362e5cff2409
enriquer2 <ecases@alfatecsistemas.es>
2018-11-28 19:58:48 +0100

    add style into more info component

--- src/body/more_info/load_info.js
+++ src/body/more_info/load_info.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './load_info.css';
 var btc_data = require('../../data/btc.json');
 var eth_data = require('../../data/eth.json');
 var xrp_data = require('../../data/xrp.json');
@@ -29,23 +30,36 @@ class Load_info extends Component {
   render() {
     return (
       <div>
-        <ul>
-          {
-            this.state.dbName.map(function(cripto){
-              return (
-                <li>
-                  Date: {cripto.date}
-                  <br />
-                  Values:
-                  <div>
-                    Open: {cripto.open} High: {cripto.high} Low: {cripto.low} Close: {cripto.close}
+        {
+          this.state.dbName.map(function(cripto){
+            return (
+              <ul>
+                <div class="older_info">
+                  <div class="row">
+                    <strong>Date</strong>: {cripto.date}
                   </div>
-                  <br></br>
-                </li>
-              )
-            })
-          }
-        </ul>
+                  <div class="row">
+                    Values:
+                  </div>
+                  <div class="older_info_values">
+                    <div class="row">
+                      <strong>Open</strong>: {cripto.open}
+                    </div>
+                    <div class="row">
+                      <strong>High</strong>: {cripto.high}
+                    </div>
+                    <div class="row">
+                      <strong>Low</strong>: {cripto.low}
+                    </div>
+                    <div class="row">
+                      <strong>Close</strong>: {cripto.close}
+                    </div>
+                  </div>
+                </div>
+              </ul>
+            )
+          })
+        }
       </div>
     );
   }
