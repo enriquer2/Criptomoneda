ce78050f754e2cd7427377e09f8ab9aed63b91d7
enriquer2 <ecases@alfatecsistemas.es>
2018-11-28 07:14:28 +0100

    add new component about more info

---
+++ src/body/more_info/more_info.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class More_info extends Component {
+  constructor(props){
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default More_info;
